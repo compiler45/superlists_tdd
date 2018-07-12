aedf50455bf47ed051933d8e4316631bbf816d78
Abraham Olaoye <abraham.program@gmail.com>
2018-07-12 21:16:14 +0100

    Template now displays a single list item, but has major flaw of not being able to show more.

--- functional_tests.py
+++ functional_tests.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -39,22 +38,26 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy GE 999 manga' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy GE 999 manga', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
         # She enters "Watch accompanying anime"
-        self.fail('Finish the test!')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Watch accompanying anime')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Page updates again, now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Watch accompanying anime', [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique URL for her --
         # some text explains this
 
         # She visits the URL - her to-do list is still there.
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
