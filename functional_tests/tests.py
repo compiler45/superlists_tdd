4f758c4f354f368a1763a04f009077c421bcee4e
Abraham Olaoye <abraham.program@gmail.com>
2018-07-14 16:45:18 +0100

    First steps of FT for layout and styling.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -123,3 +123,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy NAVI', page_text)
 
         # Satisfied, they both wallow in their virtual universes
+
+    def test_layout_and_styling(self):
+        # Lain goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        # She notices the input is nicely centered
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2,
+                               512,delta=10)
