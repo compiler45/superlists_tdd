a36e7b33409428448bec9a367ee0556d8c44ae4c
Abraham Olaoye <abraham.program@gmail.com>
2018-07-28 20:02:42 +0100

    Moved FTs into their own individual files.

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from .base import FunctionalTest
+
+MAX_WAIT = 10
+
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        # Lain goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input is nicely centered
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2,
+                               512, delta=10)
---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,26 @@
+from unittest import skip
+from selenium.common.exceptions import WebDriverException
+
+from .base import FunctionalTest
+
+
+MAX_WAIT = 10
+
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_list_items(self):
+        # Lain goes to the home page and accidentally tries to submit
+        # an empty list. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message sating
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('Write me!')
