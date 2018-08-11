521ee5ec430db274486c2320bf70b19a95cad055
Abraham Olaoye <abraham.program@gmail.com>
2018-08-11 20:37:24 +0100

    Use new form in home page, simplify tests

--- lists/views.py
+++ lists/views.py
@@ -4,19 +4,20 @@ from django.core.exceptions import ValidationError
 
 
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', { 'form': ItemForm })
 
 
 def new_list(request):
     list_ = List.objects.create()
     item = Item(text=request.POST.get('item_text', ''),
                                list=list_)
-    try: 
+    try:
         item.full_clean()
         item.save()
     except ValidationError:
