0277402dea5a03c43db05e9977377123174b837e
Abraham Olaoye <tmuxlain@gmail.com>
2018-09-11 21:01:13 +0100

    Tests now adapted for the staging server
    - Login test checks for emails from the staging server.
    - 'My List' test now saves sessions to the staging server's database for simulating authentication.

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,4 +1,7 @@
+import os
+import poplib
 import re
+import time
 
 from django.core import mail
 
@@ -17,8 +20,13 @@ class LoginTest(FunctionalTest):
         # Lain goes to the awesome Superlists site
         # she notices a "Log In" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
+        if self.staging_server:
+            test_email = 'abraham_045@hotmail.co.uk'
+        else:
+            test_email = 'lain@example.com'
+
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # A message appears telling her an email has been sent
@@ -28,15 +36,14 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(SUBJECT, email.subject)
+        body = self.wait_for_email(test_email, SUBJECT)
 
         # It has a url link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body: \n{email.body}')
+            self.fail(f'Could not find url in email body: \n{body}')
+            self.fail(f'Could not find url in email body: \n{body}')
 
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
@@ -45,10 +52,42 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # She is logged in
-        self.wait_to_be_logged_in(TEST_EMAIL)
+        self.wait_to_be_logged_in(test_email)
 
         # Now she logs out
         self.browser.find_element_by_link_text('Log out').click()
 
         # She is logged out
-        self.wait_to_be_logged_out(TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('outlook.office365.com', port=995)
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['HOTMAIL_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, _ = inbox.retr(i)
+                    lines = [l.decode('utf-8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+ 
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
